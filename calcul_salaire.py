a20c151d0c17f3cdc1dcbd7863f67f767926e933
unknown <diopwas@yahoo.fr>
2018-10-12 14:09:16 +0200

    odi

---
+++ calcul_salaire.py
@@ -0,0 +1,18 @@
+import unittest
+def calcul_salaire(metier,experience):
+    if metier == 'Architecte' and experience == 4:
+        return '4000'
+    
+    if metier == 'Médecin' and experience == 8:
+        return '7000'
+
+    if metier == 'consultant' and experience == 5:
+        return '5000'
+
+
+print(" Un architecte qui a 4 ans d'expérience gagne 4000 euros")
+print(" Un médecin qui a 8 ans d'expérience gagne 7000 euros")
+print(" Un architecte qui a 4 ans d'expérience gagne 4000 euros")
+
+
+   
